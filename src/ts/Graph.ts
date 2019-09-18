a282272837e9a0def56a2294458a4494985f980e
AChaffangeon <chaffangeon.adrien@gmail.com>
2019-09-18 16:33:53 +0200

    ac graph.ts
    can add node to the graph

--- src/ts/Graph.ts
+++ src/ts/Graph.ts
@@ -1,19 +1,21 @@
-import d3 = require("d3");
+import * as d3 from "d3-selection";
 
 export class Graph {
   svg: any;
 
   constructor() {
     this.svg = d3.select("#graph").append("svg");
-    this.svg.on("mouseup", (e: Event) => this.addNode(e))
+    this.svg.on("mouseup", (e: any) => this.addNode(e))
   }
 
-  addNode(e: Event) {
-    console.log(e)
-    /*this.svg
+  addNode(e: any) {
+    console.log(d3.event);
+    let x = d3.event.pageX;
+    let y = d3.event.pageY;
+    this.svg
       .append("circle")
         .attr("cx", x)
         .attr("cy", y)
-        .attr("r", 10)*/
+        .attr("r", 10)
   }
 }
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,4 +1,7 @@
 import { TuringMachine } from './model/TuringMachine';
+import { Graph } from './Graph';
 
 let turingMachine = new TuringMachine();
 console.log("It works!");
+
+let graph = new Graph();
