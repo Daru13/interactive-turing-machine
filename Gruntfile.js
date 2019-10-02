1056a5da07a28d0afdfc7b8de2275e9eaf943ff6
Daru13 <daru14@gmail.com>
2019-10-02 20:03:01 +0200

    Fix the issue making copyCSS act differently on OSX and Linux.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,63 +1,64 @@
 module.exports = function (grunt) {
 
-    /***************************************************************************/
-    /* Configuration of Grunt & the tasks
-    /***************************************************************************/
+  /***************************************************************************/
+  /* Configuration of Grunt & the tasks
+  /***************************************************************************/
 
-    grunt.initConfig({
-      pkg: grunt.file.readJSON("package.json"),
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
 
-      // Command line tasks
-      // They allow to run external commands (as in a terminal).
-      exec: {
-        clean: "rm -Rf build .tscache",
-        copyHTML: "cp -f ./src/index.html build/index.html",
-        copyIcons: "cp -Rf ./src/icons/ build/icons/",
-        copyCSS: "cp -Rf ./src/css/ build/css/"
-      },
+    // Command line tasks
+    // They allow to run external commands (as in a terminal).
+    exec: {
+      clean: "rm -Rf build .tscache",
+      mkDirs: "mkdir -p build/icons build/css",
+      copyHTML: "cp -f ./src/index.html build/index.html",
+      copyIcons: "cp -Rf ./src/icons/ build/icons/",
+      copyCSS: "cp -f ./src/css/*.css build/css/"
+    },
 
-      // Typescript task
-      // It compiles each TypeScript souce file into a JavaScript file.
-      ts: {
-        default : {
-          tsconfig: "./tsconfig.json"
-        }
-      },
-
-      // Browserify task
-      // It aggregates all compiled JavaScript files into a single file.
-      browserify: {
-        bundle: {
-          src: "./build/modules/index.js",
-          dest: "./build/itm.js"
-        }
+    // Typescript task
+    // It compiles each TypeScript souce file into a JavaScript file.
+    ts: {
+      default : {
+        tsconfig: "./tsconfig.json"
       }
+    },
 
-    });
+    // Browserify task
+    // It aggregates all compiled JavaScript files into a single file.
+    browserify: {
+      bundle: {
+        src: "./build/modules/index.js",
+        dest: "./build/itm.js"
+      }
+    }
 
-
-    /***************************************************************************/
-    /* npm plugin loading
-    /***************************************************************************/
-
-    grunt.loadNpmTasks("grunt-exec");
-    grunt.loadNpmTasks("grunt-ts");
-    grunt.loadNpmTasks("grunt-browserify");
-
-    /***************************************************************************/
-    /* Task registering
-    /***************************************************************************/
-
-    grunt.registerTask("clean",
-        "Remove the 'build' directory and clean temp/cache directories.",
-        "exec:clean"
-    );
-
-    grunt.registerTask("build",
-        "Compile Typescript sources, Browserify them and copy static sources into the 'build' directory.",
-        ["ts", "browserify", "exec:copyHTML", "exec:copyCSS", "exec:copyIcons"]
-    );
+  });
 
 
-    grunt.registerTask("default", "build");
-  };
+  /***************************************************************************/
+  /* npm plugin loading
+  /***************************************************************************/
+
+  grunt.loadNpmTasks("grunt-exec");
+  grunt.loadNpmTasks("grunt-ts");
+  grunt.loadNpmTasks("grunt-browserify");
+
+  /***************************************************************************/
+  /* Task registering
+  /***************************************************************************/
+
+  grunt.registerTask("clean",
+    "Remove the 'build' directory and clean temp/cache directories.",
+    "exec:clean"
+  );
+
+  grunt.registerTask("build",
+    "Compile Typescript sources, Browserify them and copy static sources into the 'build' directory.",
+    ["ts", "browserify", "exec:mkDirs", "exec:copyHTML", "exec:copyCSS", "exec:copyIcons"]
+  );
+
+
+  grunt.registerTask("default", "build");
+};
