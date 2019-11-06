da3728d52b8792dbc718245d060bc7c083422f61
Daru13 <daru14@gmail.com>
2019-11-06 04:54:34 +0100

    Add a shorter name for the no-transition error.

--- src/ts/errors/NoTransitionAvailableError.ts
+++ src/ts/errors/NoTransitionAvailableError.ts
@@ -9,7 +9,8 @@ export class NoTransitionAvailableError extends TMError {
         super(turingMachine,
             `No transition available in state ${state.getLabel()}`, 
             `There is no transition going out of state ${state.getLabel()} for symbol "${turingMachine.tape.getCurrentSymbol()}": the Turing machine is stuck!`,
-            `Either add more transitions or make state ${state.getLabel()} final to tell the Turing machine it can stop here (you can click on a state to change whether it is final or not).`
+            `Either add more transitions or make state ${state.getLabel()} final to tell the Turing machine it can stop here (you can click on a state to change whether it is final or not).`,
+            "No transition available"
         );
 
         this.state = state;
